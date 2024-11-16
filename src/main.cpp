039ab9539417319cf48f9f27375808cb32f47ceb
Sank7383 <kushwahasandeepshiv041@gmail.com>
2024-11-16 15:15:00 +0530

    auto count start and millis changes

---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+#include <Arduino.h>
+#include <TM1637Display.h>
+
+#define CLK 14  // Define the CLK pin for TM1637
+#define DIO 13  // Define the DIO pin for TM1637
+#define IR_PIN 12 // Define the IR sensor pin
+
+TM1637Display display(CLK, DIO);
+
+volatile int count = 0; // Initialize the count variable (volatile for ISR)
+bool lastState = HIGH; // Variable to hold the last state of the IR sensor
+
+volatile u_int32_t temp ;
+u_int32_t start ;
+bool flag ;
+// Interrupt Service Routine (ISR) for counting objects
+void IRAM_ATTR countObject() {
+        if(flag==0)
+    {
+        flag =1;
+        start = millis();
+    }
+    count++; // Increment count when an object is detected
+    temp = millis() ;
+}
+
+void setup() {
+    Serial.begin(115200);
+    display.setBrightness(0x0f); // Set maximum brightness
+    pinMode(IR_PIN, INPUT); // Set the IR pin as input
+
+    // Attach interrupt to the IR_PIN for falling edge detection
+    attachInterrupt(digitalPinToInterrupt(IR_PIN), countObject, RISING);
+    millis();
+}
+
+void loop() {
+    // Display the count on TM1637
+    if(flag == 1)
+    {
+         display.showNumberDec(count); // Update display in the main loop
+    }
+    if(millis()- temp >3000 && flag == 1)
+    {
+        u_int32_t tm = temp - start ; 
+        Serial.print("time is : ");
+        Serial.println(tm);
+        Serial.println("Count is");
+        Serial.println(count);
+        flag= 0 ;
+        count = 0 ;
+       
+    }
+
+}
