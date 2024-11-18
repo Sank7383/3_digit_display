a33edafcd7ee02a52098204956a02cda0dffd2de
Sank7383 <kushwahasandeepshiv041@gmail.com>
2024-11-18 10:50:30 +0530

    added code to desplay meter and serial put on debug mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <TM1637Display.h>
 
+ #define debug
+
 #define CLK 14  // Define the CLK pin for TM1637
 #define DIO 13  // Define the DIO pin for TM1637
 #define IR_PIN 12 // Define the IR sensor pin
@@ -9,10 +11,12 @@ TM1637Display display(CLK, DIO);
 
 volatile int count = 0; // Initialize the count variable (volatile for ISR)
 bool lastState = HIGH; // Variable to hold the last state of the IR sensor
+ volatile double length ;
 
 volatile u_int32_t temp ;
 u_int32_t start ;
 bool flag ;
+
 // Interrupt Service Routine (ISR) for counting objects
 void IRAM_ATTR countObject() {
         if(flag==0)
@@ -33,23 +37,32 @@ void setup() {
     attachInterrupt(digitalPinToInterrupt(IR_PIN), countObject, RISING);
     millis();
 }
-
+ u_int32_t tm;
 void loop() {
     // Display the count on TM1637
+        if(flag==1)
+    {
+        length = count * 4.1 ;
+        length = length/100 ;
+        display.showNumberDec(u_int32_t(length));
+    }
     if(flag == 1)
     {
-         display.showNumberDec(count); // Update display in the main loop
+        // display.showNumberDec(count); // Update display in the main loop
     }
     if(millis()- temp >3000 && flag == 1)
     {
-        u_int32_t tm = temp - start ; 
+        #ifdef debug
+        tm = temp - start ; 
         Serial.print("time is : ");
         Serial.println(tm);
         Serial.println("Count is");
         Serial.println(count);
+        Serial.println(length); 
+        #endif
         flag= 0 ;
-        count = 0 ;
-       
+        count = 0 ;  
+        
     }
 
 }
